b59ab358201dacf8201f7c687b19a416b7e2a9c4
Colin Campbell <prontobol@gmail.com>
2020-03-23 00:36:53 -0500

    Test

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express')
 const app = express();
 
 const staticPath = __dirname+'/static';
+/**
 app.use('/',express.static(staticPath))
 
 app.get("/*",(req,res)=>
 {
     //res.sendFile(staticPath+"/index.html");
     res.json({mess:"Hello World"})
+})**/
+
+app.get('/',(req,res)=>{
+    res.json({mess:"Heya"})
 })
 
-app.listen(3000 || process.env.PORT,(err)=>{
+const port = process.env.PORT || 3000;
+console.log(port);
+app.listen(port,(err)=>{
     if (err) throw err;
     console.log("Server Started");
 })
