add9c0ffbf760b0673dc84fa3bdcbfb24bd22662
Colin Campbell <prontobol@gmail.com>
2020-04-05 12:06:55 -0500

    Added document route to get JSON

--- routes/documents.js
+++ routes/documents.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('./../config/db')
 
-router.get('/:artID',(req,res)=>{
+// Get documents from artifact
+router.get('/from-art/:artID',(req,res)=>{
     let artifactID = req.params.artID;
-    db.query('SELECT * FROM documents WHERE art_id = $1',[artifactID],
+    // write code to check on the user id, prevent any other user from getting access to the resource purposefully or accidentally
+    db.query('SELECT doc_id, version, comment, user_id, date_uploaded, date_modified, art_id, type FROM documents WHERE art_id = $1',[artifactID],
     (err,result)=>{
         if (err) throw err;
         let docs = result.rows;
