718fe342971d65fd3f6a66be235c078643b95232
Colin Campbell <prontobol@gmail.com>
2020-04-21 14:52:05 -0500

    Minor fix

--- routes/member.js
+++ routes/member.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('./../config/db')
 
-
 router.get('/all',(req,res)=>{
     console.log(req.session)
     let orgID = req.session.orgInfo.org_id;
