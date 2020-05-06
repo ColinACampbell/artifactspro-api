323aa1cbc7c1c80b8fae6ce7526cd86010e12714
Colin Campbell <prontobol@gmail.com>
2020-05-05 21:21:12 -0500

    Rm space

--- routes/member.js
+++ routes/member.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const memberContoller = require('./../controllers/members')
 
-
 router.get('/all',memberContoller.getAll)
 
 router.get('/invite-code',memberContoller.getInviteCode)
