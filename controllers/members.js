17b2d4dec7febcc6d8a9f51e3e1e8850eed036d0
Colin Campbell <prontobol@gmail.com>
2020-05-05 21:20:50 -0500

    Refractor module

---
+++ controllers/members.js
@@ -0,0 +1,27 @@
+const db = require('./../config/db');
+const config = require('./../config/config')
+
+exports.getAll = (req,res)=>{
+    //console.log(req.session)
+    let orgID = req.session.orgInfo.org_id;
+    db.query(`select role, email, first_name, last_name from organization_members
+    INNER JOIN users on users.user_id = organization_members.user_id
+    where org_id  = $1`,[orgID],
+    (err,result)=>{
+        if (err) throw err;
+        //console.log(result.rows);
+        res.json(result.rows)
+    })
+}
+
+exports.getInviteCode = (req,res)=>{
+    let orgID = req.session.orgInfo.org_id;
+    db.query('SELECT * FROM organizations WHERE org_id = $1',[orgID],(err,result)=>{
+        console.log(result.rows[0].org_code);
+        let orgCode = result.rows[0].org_code;
+        let orgName = result.rows[0].name;
+        let serverhost = config.dev ? 'http://localhost:4200': config.host
+        let invite_url = `${serverhost}/team/invite/${orgCode}`
+        res.json({invite_url}); 
+    })
+}
