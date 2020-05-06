a32ec39adcde3f96f2dea69ff39a7e41f067b423
Colin Campbell <prontobol@gmail.com>
2020-05-05 21:13:34 -0500

    Refractored code

---
+++ controllers/documents.js
@@ -0,0 +1,103 @@
+const db = require('./../config/db');
+const fs = require('fs');
+const path = require('path')
+const config = require('./../config/config')
+
+const fileTypes = {
+    'application/msword': 'doc',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+    'image/jpeg': 'jpg',
+    'image/png' : 'png'
+};
+
+exports.docFromArtFromID = (req, res) => {
+    let artifactID = req.params.artID;
+    // write code to check on the user id, prevent any other user from getting access to the resource purposefully or accidentally
+    db.query('SELECT doc_id, version, comment, user_id, date_uploaded, date_modified, art_id, type FROM documents WHERE art_id = $1', [artifactID],
+        (err, result) => {
+            if (err) throw err;
+            let docs = result.rows;
+            res.status(200).json(docs);
+        })
+}
+
+exports.upload = (req, res) => {
+
+    let artID = req.params.artID;
+    let userID = req.session.userInfo.user_id;
+    let dateModified = req.body.date_modified;
+    let dateUploaded = req.body.date_uploaded;
+    let data = req.body.data;
+    let version = req.body.version;
+    let comment = req.body.comment;
+    let fileType = req.body.file_type;
+
+    let base64Data = data.split(',')[1];
+
+    let buffer = Buffer.from(base64Data, "base64"); // convert the file buffer
+    //fs.writeFileSync(`temp/${version}.${fileTypes[fileType]}`, buffer);
+
+    db.query(`INSERT INTO documents
+    ("version", "comment", user_id, "data", date_uploaded, date_modified, art_id, "type")
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8);
+    `, [version, comment, userID, buffer, dateUploaded, dateModified, artID, fileType],
+        (err, result) => {
+            if (err) throw err;
+
+            //fs.mkdirSync('temp')
+            res.status(200).json({ message: 'ok' });
+        })
+}
+
+exports.preview = (req, res) => {
+    let doc = req.params.docName;
+    let artID = req.params.artID;
+    const link = path.join(__dirname, `../docs/preview/${artID}/${doc}`);
+    res.sendFile(link);
+}
+
+exports.getLink = (req, res, next) => {
+
+    let code = 200
+    if (!req.session.userInfo)
+        code = 401;
+    // TODO : AUTH user
+
+    const artID = req.params.artID;
+    const docID = req.params.docID;
+    const dir = `./docs/preview/${artID}`; // the directory for which the doc will be in
+
+    if (!fs.existsSync(dir))
+        fs.mkdirSync(dir, { recursive: true });
+
+
+    // doc id will act as the file name with the extenstion so docid.file_ext
+    db.query('SELECT * FROM documents WHERE doc_id = $1', [docID],
+        (err, results) => {
+
+            if (err) throw err;
+
+            let doc = results.rows[0];
+
+            let filePath = `${dir}/${docID}.${fileTypes[doc.type]}`
+
+            if (!fs.existsSync(filePath))
+                fs.writeFileSync(filePath, doc.data);
+
+            // TODO Change this value whenever pushing to production
+            // TODO make this more automated
+            let serverhost = config.dev ? 'http://localhost:3000': config.host
+            res.status(code).json({
+                download: `${serverhost}/api/docs/preview/${artID}/${docID}.${fileTypes[doc.type]}`
+            });
+
+        })
+}
+
+exports.deleteDocument = (req,res)=>{
+    const docID = req.params.docID;
+    db.query('DELETE FROM documents WHERE doc_id = $1',[docID],(err,result)=>{
+        if (err) throw err;
+        res.json({message:'done'})
+    })
+}
--- routes/documents.js
+++ routes/documents.js
@@ -1,112 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../config/db');
-const fs = require('fs');
-const path = require('path')
-const config = require('./../config/config')
-
-const fileTypes = {
-    'application/msword': 'doc',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
-    'image/jpeg': 'jpg',
-    'image/png' : 'png'
-};
+const documentController = require('./../controllers/documents')
 
 // Get documents from artifact
-router.get('/from-art/:artID', (req, res) => {
-    let artifactID = req.params.artID;
-    // write code to check on the user id, prevent any other user from getting access to the resource purposefully or accidentally
-    db.query('SELECT doc_id, version, comment, user_id, date_uploaded, date_modified, art_id, type FROM documents WHERE art_id = $1', [artifactID],
-        (err, result) => {
-            if (err) throw err;
-            let docs = result.rows;
-            res.status(200).json(docs);
-        })
-})
+router.get('/from-art/:artID',documentController.docFromArtFromID);
 
-router.post('/upload/:artID', (req, res) => {
+router.post('/upload/:artID',documentController.upload);
 
-    let artID = req.params.artID;
-    let userID = req.session.userInfo.user_id;
-    let dateModified = req.body.date_modified;
-    let dateUploaded = req.body.date_uploaded;
-    let data = req.body.data;
-    let version = req.body.version;
-    let comment = req.body.comment;
-    let fileType = req.body.file_type;
-
-    let base64Data = data.split(',')[1];
-
-    let buffer = Buffer.from(base64Data, "base64"); // convert the file buffer
-    //fs.writeFileSync(`temp/${version}.${fileTypes[fileType]}`, buffer);
-
-    db.query(`INSERT INTO documents
-    ("version", "comment", user_id, "data", date_uploaded, date_modified, art_id, "type")
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8);
-    `, [version, comment, userID, buffer, dateUploaded, dateModified, artID, fileType],
-        (err, result) => {
-            if (err) throw err;
-
-            //fs.mkdirSync('temp')
-            res.status(200).json({ message: 'ok' });
-        })
-})
-
-const unoconv = require('awesome-unoconv');
-router.post("/link/:artID/:docID", (req, res, next) => {
-
-    let code = 200
-    if (!req.session.userInfo)
-        code = 401;
-    // TODO : AUTH user
-
-    const artID = req.params.artID;
-    const docID = req.params.docID;
-    const dir = `./docs/preview/${artID}`; // the directory for which the doc will be in
-
-    if (!fs.existsSync(dir))
-        fs.mkdirSync(dir, { recursive: true });
-
-
-    // doc id will act as the file name with the extenstion so docid.file_ext
-    db.query('SELECT * FROM documents WHERE doc_id = $1', [docID],
-        (err, results) => {
-
-            if (err) throw err;
-
-            let doc = results.rows[0];
-
-            let filePath = `${dir}/${docID}.${fileTypes[doc.type]}`
-
-            if (!fs.existsSync(filePath))
-                fs.writeFileSync(filePath, doc.data);
-
-            // TODO Change this value whenever pushing to production
-            // TODO make this more automated
-            let serverhost = config.dev ? 'http://localhost:3000': config.host
-            res.status(code).json({
-                download: `${serverhost}/api/docs/preview/${artID}/${docID}.${fileTypes[doc.type]}`
-            });
-
-        })
-})
-
+router.post("/link/:artID/:docID",documentController.getLink)
 
 // @ :doc = docID + file extension
-router.get('/preview/:artID/:docName', (req, res) => {
-    let doc = req.params.docName;
-    let artID = req.params.artID;
-    const link = path.join(__dirname, `../docs/preview/${artID}/${doc}`);
-    res.sendFile(link);
-})
+router.get('/preview/:artID/:docName', documentController.preview)
 
-
-router.delete('/delete/:docID',(req,res)=>{
-    const docID = req.params.docID;
-    db.query('DELETE FROM documents WHERE doc_id = $1',[docID],(err,result)=>{
-        if (err) throw err;
-        res.json({message:'done'})
-    })
-})
+router.delete('/delete/:docID',documentController.deleteDocument)
 
 module.exports = router;
